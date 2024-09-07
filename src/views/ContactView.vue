f1f910a3ed630e4df3a15259bd936e39106deba7
coteensignia <mjensignia@gmail.com>
2024-09-07 18:28:40 -0400

    Suricata_router

---
+++ src/views/ContactView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1 class=" d-flex justify-content-center mt-5">Contacto</h1>
+    <div class="d-flex justify-content-center">
+  <button @click="VolverAlInicio" class="btn btn-info mb-4">Ir a Inicio</button>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    VolverAlInicio() {
+      this.$router.push({ name: 'inicio' })
+    }
+  }
+}
+</script>
---
+++ src/views/ProductosView.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="container" id="productos">
+    <h1 class="text-center">Productos</h1>
+    <div class="d-flex justify-content-center">
+  <button @click="VolverAlInicio" class="btn btn-info mb-4">Ir a Inicio</button>
+</div>
+
+    <div class="row">
+      <div class="col-sm-4 col-12 px-5 mb-4">
+      <div class="card">
+          <img src="../assets/img/desayunos.jpg" class="card-img-top" alt="colaciones" />
+          <div class="card-body">
+            <h5 class="card-title fw-bold">{{ nombre }}</h5>
+            <p class="card-text">
+              {{ descripcion }}
+            </p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Precio: {{ precio }}</li>
+            <li class="list-group-item">Cantidad: {{ cantidad }}</li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">Ver más</a>
+            <a href="#" class="card-link">Comprar</a>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  
+  </section>
+</template>
+<script>
+export default {
+  methods: {
+    VolverAlInicio() {
+      this.$router.push({ name: 'inicio' })
+    }
+  },
+
+  props: {
+    nombre: {
+      type: String,
+      required: true
+    },
+    descripcion: {
+      type: String,
+      required: true
+    },
+    precio: {
+      type: Number,
+      required: true
+    },
+    cantidad: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
